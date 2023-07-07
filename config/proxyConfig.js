9b083cbb918f309fdfc666b8671f3fdae647edc7
FlyingIdeal <963161475@qq.com>
2023-07-07 15:25:43 +0800

    fix: 代理

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,18 +1,18 @@
 module.exports = {
-	'/api': {
-		target: 'http://127.0.0.1:8899/api', // 代理到本地环境
-		// target: 'http://admin.dev.deemob.com/api', // 代理到dev环境
-		// target: 'https://admin.gray.deemob.com/api', // 代理到gray环境
+	'/api': { // 接口代理
+		target: 'http://apia.gladdigit.com/api', // 代理到本地环境
+		// target: 'http://grayapia.gladdigit.com/api', // 代理到本地环境
 		changeOrigin: true,
 		pathRewrite: {
 			'^/api': ''
 		}
 	},
-	'/mock': {
-		target: 'http://127.0.0.1:8081/mock', // 代理到dev环境
+	'/xls': { // 下载文档代理
+		target: 'http://apia.gladdigit.com/xls', // 代理到本地环境
+		// target: 'http://grayapia.gladdigit.com/xls', // 代理到本地环境
 		changeOrigin: true,
 		pathRewrite: {
-			'^/mock': ''
+			'^/xls': ''
 		}
 	}
 }
